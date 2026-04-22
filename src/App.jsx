2d97dd30e7542195a6b78a83b4244a52d546e99d
Juan Jose Morales <juanjoseotero993@gmail.com>
2026-04-21 19:04:52 -0500

    inventario

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Almacenamiento from './Componetes/Inventario/Almacenamiento/Almacenamiento'
+// import Alerta_Stock from './Componetes/Inventario/Alerta_Stock/Alerta_Stock'
+import Control_Inventario from './Componetes/Inventario/Control_Inventario/Control_Inventario'
+
+const App = () => {
+  return (
+    <>
+      {/* <Almacenamiento/> */}
+      {/* <Alerta_Stock/> */}
+      <Control_Inventario/>
+    </>
+  )
+}
+export default App
