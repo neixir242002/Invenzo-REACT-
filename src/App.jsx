1777c5025f193f3ee6e72d922909f5c58aaa19ad
Neixir <palaciosmateo080@gmail.com>
2026-04-23 16:16:18 -0500

    vistas juntas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 //import React from 'react'
-//import Home from './pages/Home'
+import Home from './pages/Home'
 import Login from './pages/Login';
 
 const App = () => {
   return (
     <div>
-      {/*<Home />*/}
+      <Home />
       <Login />
 
     </div>
